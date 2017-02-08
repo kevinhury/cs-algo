a08015c2039984802fa3cc7b2ae56d66e16a9204
Kevin Hury <kevinh306@gmail.com>
2017-02-09 00:15:01 +0200

    create doubly linked list interface

---
+++ Doubly Linked List/doublylinkedlist.h
@@ -0,0 +1,24 @@
+#ifndef doublylinkedlist_h
+#define doublylinkedlist_h
+
+#include <stdio.h>
+
+typedef void* data_t;
+
+typedef struct {
+    data_t value;
+    struct node *prev;
+    struct node *next;
+} node;
+
+node *createnode(data_t value);
+void iterate(node *head, void (*iterator)(const void *value));
+void append(node *head, data_t value);
+void prepend(node **head, data_t value);
+data_t extract(node *head);
+data_t remove_first(node **head);
+data_t remove_by_index(node **head, int index);
+int remove_by_value(node **head, data_t value);
+void freelist(node **head);
+
+#endif /* doublylinkedlist_h */
