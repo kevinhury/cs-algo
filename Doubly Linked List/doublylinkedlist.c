94e777051d87d77433d0fe9d4b0dbdd0e2b29eeb
Kevin Hury <kevinh306@gmail.com>
2017-02-09 00:30:10 +0200

    Implemented double linked list

--- Doubly Linked List/doublylinkedlist.c
+++ Doubly Linked List/doublylinkedlist.c
@@ -41,3 +41,99 @@ void prepend(node **head, data_t value)
     (*head)->prev = n;
     *head = n;
 }
+
+data_t extract(node *head)
+{
+    node *current;
+    data_t retval;
+    if (head->next != NULL)
+    {
+        retval = head->value;
+        free(head);
+        return retval;
+    }
+    
+    current = head;
+    while (current->next->next != NULL)
+    {
+        current = current->next;
+    }
+    
+    retval = current->next->value;
+    free(current->next);
+    current->next = NULL;
+    return retval;
+}
+
+data_t remove_first(node **head)
+{
+    node *next;
+    data_t retval = 0;
+    if (*head == NULL) return retval;
+    
+    next = (*head)->next;
+    next->prev = NULL;
+    retval = (*head)->value;
+    free(*head);
+    *head = next;
+    
+    return retval;
+}
+
+int remove_by_value(node **head, data_t value)
+{
+    int retval = 0;
+    node *current = *head;
+    node *temp = NULL;
+    
+    if (current->value == value) {
+        remove_first(head);
+        return retval;
+    }
+    
+    while (current->next->value != value)
+    {
+        if (current->next == NULL) return -1;
+        current = current->next;
+        retval++;
+    }
+    
+    current->next->next->prev = current;
+    temp = current->next;
+    current->next = temp->next;
+    free(temp);
+    
+    return retval;
+}
+
+data_t remove_by_index(node **head, int index)
+{
+    int i = 0;
+    data_t retval = 0;
+    node *current = *head;
+    node *temp = NULL;
+    
+    if (index == 0) return remove_first(head);
+    
+    for (i = 0; i < index - 1; i++)
+    {
+        if (current->next == NULL) return retval;
+        current = current->next;
+    }
+    
+    current->next->next->prev = current;
+    temp = current->next;
+    retval = temp->value;
+    current->next = temp->next;
+    free(temp);
+    
+    return retval;
+}
+
+void freelist(node **head)
+{
+    if (*head == NULL) return;
+    freelist(&((*head)->next));
+    free(*head);
+    *head = NULL;
+}
