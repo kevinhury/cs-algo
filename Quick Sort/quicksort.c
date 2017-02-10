2afee05745cf2c200ae9cdae55bc085dd71b7a39
Kevin Hury <kevinh306@gmail.com>
2017-02-10 10:42:33 +0200

    Implemented quick sort algorithm

---
+++ Quick Sort/quicksort.c
@@ -0,0 +1,39 @@
+#include "quicksort.h"
+
+
+void swap(int *i, int *j)
+{
+    int temp = *i;
+    *i = *j;
+    *j = temp;
+}
+
+int partition(data_t *arr, int low, int high)
+{
+    int pivot, i, j;
+    pivot = arr[high];
+    i = low - 1;
+    
+    for (j = low; j <= high - 1; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    
+    return i + 1;
+}
+
+void quicksort(data_t *arr, int low, int high)
+{
+    if (low >= high) return;
+    
+    int pivot = partition(arr, low, high);
+    
+    quicksort(arr, low, pivot - 1);
+    quicksort(arr, pivot + 1, high);
+}
+
---
+++ Quick Sort/quicksort.h
@@ -0,0 +1,10 @@
+#ifndef quicksort_h
+#define quicksort_h
+
+#include <stdio.h>
+
+typedef int data_t;
+
+void quicksort(data_t *arr, int low, int high);
+
+#endif /* quicksort_h */
