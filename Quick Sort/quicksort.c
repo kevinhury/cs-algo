d8d476406675549c6789b1eca7e17696797ca304
Kevin Hury <kevinh306@gmail.com>
2017-02-11 13:50:57 +0200

    Added documentation for quicksort algorithm

--- Quick Sort/quicksort.c
+++ Quick Sort/quicksort.c
@@ -31,9 +31,9 @@ void quicksort(data_t *arr, int low, int high)
 {
     if (low >= high) return;
     
-    int pivot = partition(arr, low, high);
+    int part = partition(arr, low, high);
     
-    quicksort(arr, low, pivot - 1);
-    quicksort(arr, pivot + 1, high);
+    quicksort(arr, low, part - 1);
+    quicksort(arr, part + 1, high);
 }
 
--- Quick Sort/quicksort.h
+++ Quick Sort/quicksort.h
@@ -5,6 +5,18 @@
 
 typedef int data_t;
 
+/**
+ * Quick Sort algorithm.
+ * 
+ * @param arr Pointer to an array of data_t. quicksort will modify the given array.
+ * @param low The lowest index to sort from.
+ * @param high The highest index to sort to.
+ *
+ * Example usage:
+ * @code
+ *    quicksort(array_pointer, 0, array_size - 1);
+ * @endcode
+ */
 void quicksort(data_t *arr, int low, int high);
 
 #endif /* quicksort_h */
