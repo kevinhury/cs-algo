b2e8350cc013c50abf9d28fb7912828b9f184991
Kevin Hury <kevinh306@gmail.com>
2017-02-11 13:51:27 +0200

    Added documentation for merge_sort an bubblesort

--- Bubble Sort/bubblesort.h
+++ Bubble Sort/bubblesort.h
@@ -5,6 +5,17 @@
 
 typedef int data_t;
 
+/**
+ * Bubble Sort algorithm.
+ *
+ * @param arr Pointer to an array of data_t. bubblesort will modify the given array.
+ * @param size The given array size.
+ *
+ * Example usage:
+ * @code
+ *    bubblesort(array_pointer, array_size);
+ * @endcode
+ */
 void bubblesort(data_t *arr, int size);
 
 #endif /* bubblesort_h */
--- Merge Sort/mergesort.h
+++ Merge Sort/mergesort.h
@@ -3,6 +3,18 @@
 
 #include <stdio.h>
 
-void merge_sort(int *arr, int left, int right);
+/**
+ * Merge Sort algorithm.
+ *
+ * @param arr Pointer to an array of data_t. merge_sort will modify the given array.
+ * @param low The lowest index to sort from.
+ * @param high The highest index to sort to.
+ *
+ * Example usage:
+ * @code
+ *    merge_sort(array_pointer, 0, array_size - 1);
+ * @endcode
+ */
+void merge_sort(int *arr, int low, int high);
 
 #endif /* mergesort_h */
