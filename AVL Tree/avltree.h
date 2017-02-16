b6d0694b13a25d63422da80a8b67379bb25daae9
Kevin Hury <kevinh306@gmail.com>
2017-02-16 22:20:27 +0200

    Added docs for all tree

--- AVL Tree/avltree.h
+++ AVL Tree/avltree.h
@@ -4,8 +4,30 @@
 #include <stdio.h>
 #include "binarysearchtree.h"
 
+/**
+ * Returns the balance from a given node.
+ * In other words it returns the difference between the branches diameters.
+ *
+ * @param bst The tree's node.
+ *
+ * @return The difference between the branches diameters.
+ */
 int getbalance(bst_node *bst);
+
+/**
+ * Balances the tree so that the two child subtrees of any node differ by at most one.
+ * Balancing is done by peforming tree rotations.
+ *
+ * @param bst The tree's node.
+ */
 void avlbalance(bst_node **bst);
+
+/**
+ * Inserts a new node to the tree while maintaining its balance.
+ * 
+ * @param bst The tree's node.
+ * @param value Value to insert.
+ */
 void avlinsert(bst_node **bst, data_t value);
 
 #endif /* avltree_h */
