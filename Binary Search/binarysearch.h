f2cbe6a94ad24858f765182ec3ab452bbc87d13b
Kevin Hury <kevinh306@gmail.com>
2017-02-18 16:09:45 +0200

    added docs for binary search

--- Binary Search/binarysearch.h
+++ Binary Search/binarysearch.h
@@ -5,6 +5,20 @@
 
 typedef int data_t;
 
+/**
+ * Binary Search algorithm.
+ *
+ * @param arr Pointer to an array of data_t.
+ * @param size The array size.
+ * @param value Value to search in the array. Returns -1 if not found.
+ *
+ * @return The index of the given value. -1 if not found.
+ *
+ * Example usage:
+ * @code
+ *    binarysearch(array_pointer, 10, 2);
+ * @endcode
+ */
 int binarysearch(data_t *arr, int size, data_t value);
 
 #endif /* binarysearch_h */
