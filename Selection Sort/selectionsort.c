5ffe403a97539499d5eaf23e0729250112cec04f
Kevin Hury <kevinh306@gmail.com>
2017-02-25 13:47:41 +0200

    Implemented selection sort

---
+++ Selection Sort/selectionsort.c
@@ -0,0 +1,17 @@
+#include "selectionsort.h"
+
+void selectionsort(int *arr, int size)
+{
+    int i, j, temp;
+    for (i = 0; i < size; i++)
+    {
+        for (j = i + 1; j < size; j++)
+        {
+            if (arr[j] < arr[i]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
---
+++ Selection Sort/selectionsort.h
@@ -0,0 +1,8 @@
+#ifndef selectionsort_h
+#define selectionsort_h
+
+#include <stdio.h>
+
+void selectionsort(int *arr, int size);
+
+#endif /* selectionsort_h */
